737e81054fcb66967b3aa7b059245c9eab9edfc9
Figamus <fig74656@yahoo.com>
2018-10-29 10:23:21 -0500

    Created the Product Model to the specifications of the ERD

---
+++ BangazonAPI/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonAPI.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int MerchantId { get; set;  }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public int Quantity { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public int ProductTypeId { get; set;  }
+    }
+}
