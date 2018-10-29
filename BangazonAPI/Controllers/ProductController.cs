db19bf313eb741d55cd54ad01fa50bf29146b4b8
Figamus <fig74656@yahoo.com>
2018-10-29 11:14:28 -0500

    Fixed an error in the product model that did not match the table

---
+++ BangazonAPI/Controllers/ProductController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using BangazonAPI.Models;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace BangazonAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public ProductController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public IDbConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+
+        // GET: api/<controller>
+        [HttpGet]
+        public async Task<IActionResult> Get(string q)
+        {
+            string sql = @"
+            SELECT 
+                p.Id
+                p.ProductTypeId
+                p.CustomerId
+                p.Price
+                p.Title
+                p.Description
+                p.Quantity
+            FROM Product P
+            WHERE 1=1";
+
+            if (q != null)
+            {
+                string isQ = $@"
+                    AND p.Title LIKE '%{q}%'
+                ";
+                sql = $"{sql} {isQ}";
+            }
+
+            using (IDbConnection conn = Connection)
+            {
+
+                IEnumerable<Product> products = await conn.QueryAsync<Product>(sql);
+                return Ok(products);
+            }
+        }
+
+        // GET api/<controller>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
