66bd4da3da672d0cf64c4105e76367483cbe80db
Kelly Cook <kellycook20878@gmail.com>
2018-10-30 11:27:00 -0500

    finished department and commented on concepts I was unsure on

--- BangazonAPI/Controllers/DepartmentController.cs
+++ BangazonAPI/Controllers/DepartmentController.cs
@@ -17,7 +17,6 @@ namespace BangazonAPI.Controllers
     public class DepartmentController : ControllerBase
     {
         private readonly IConfiguration _config;
-        private object conn;
 
         public DepartmentController(IConfiguration config)
         {
@@ -33,16 +32,21 @@ namespace BangazonAPI.Controllers
         }
 
         [HttpGet]
+        /* Below you need the strings of _include, _filter, and _gt (greater than)
+        This is for the section where I am looking for employees in the departments */
         public async Task<IActionResult> Get(string _include, string _filter, int _gt)
         {
             using (IDbConnection conn = Connection)
             {
                 string sql = "SELECT * FROM Department";
-
+                /* if statement including employees */
                 if (_include == "employees")
                 {
-                    Dictionary<int, Department> listOfDepartments = new Dictionary<int, Department>();
+                    /* Creating a dictionary of departments titled "listOfDepartments */
+                    Dictionary<int, Department> departmentList = new Dictionary<int, Department>();
 
+                    /* SQL = this list. dpt = department and e = employee. Includes the properties from each class
+                    The employee has a department ID and that is where the employee and department class join */
                     sql = @"SELECT
                             dpt.Id,
                             dpt.Name,
@@ -54,37 +58,47 @@ namespace BangazonAPI.Controllers
                             e.IsSupervisor
                             FROM Department dpt
                             JOIN Employee e ON dpt.Id = e.DepartmentId";
-
+                    /* Creating a variable named "departmentEmployees. This is where the dapper magic happens
+                    This is kind of like a foreach loop. It's looking for where the employee and department match and it
+                    is adding that to the dictionary and building it up. */
                     var departmentEmployees = await conn.QueryAsync<Department, Employee, Department>(sql,
                         (department, employee) =>
                         {
-                            if (!listOfDepartments.ContainsKey(department.Id))
+                            if (!departmentList.ContainsKey(department.Id))
                             {
-                                listOfDepartments[department.Id] = department;
+                                departmentList[department.Id] = department;
                             }
-                            listOfDepartments[department.Id].EmployeeList.Add(employee);
+                            departmentList[department.Id].EmployeeList.Add(employee);
                             return department;
                         });
-                    return Ok(listOfDepartments.Values);
+                    /* Returning the results of the departmentList */
+                    return Ok(departmentList.Values);
                 }
 
+                /* For finding a budget. Steve asks for something that is greater than 300,000 but we intially did not populate the database with a 
+                number greater than that. So I put 100,000 */
                 if (_filter == "budget" && _gt >= 100000)
+                    /* Looking into the SQL wfrom where the department budget is greater than or equal to 100,000 */
                 {
 
                     sql = $@"SELECT * FROM Department WHERE Budget >= {_gt}";
 
                 }
-
+                /* The variable departmentBudget is using dapper to execute the SQL */
                 var departmentBudget = await conn.QueryAsync<Department>(sql);
+                /* Returning the values */
                 return Ok(departmentBudget);
             }
         }
 
         // GET api/department/5
         [HttpGet("{id}", Name = "GetDepartment")]
+        /* Getting a department by an ID */
         public async Task<IActionResult> Get([FromRoute]int id)
         {
-            string sql = $@"
+            using (IDbConnection conn = Connection)
+            {
+                string sql = $@"
             SELECT
                 dpt.Id,
                 dpt.Name,
@@ -92,11 +106,9 @@ namespace BangazonAPI.Controllers
             FROM Department dpt
             WHERE dpt.Id = {id}
             ";
+                var SingleDepartment = (await conn.QueryAsync<Department>(sql)).Single();
+                return Ok(SingleDepartment);
 
-            using (IDbConnection conn = Connection)
-            {
-                IEnumerable<Department> departments = await conn.QueryAsync<Department>(sql);
-                return Ok(departments);
             }
         }
 
@@ -121,15 +133,14 @@ namespace BangazonAPI.Controllers
             }
         }
 
-        // PUT api/paymentType/5
+        // PUT api/department/5
         [HttpPut("{id}")]
-        public async Task<IActionResult> Put(int id, [FromBody] PaymentType paymentType)
+        public async Task<IActionResult> Put(int id, [FromBody] Department department)
         {
             string sql = $@"
-            UPDATE PaymentType
-            SET AcctNumber = '{paymentType.AcctNumber}',
-                Name = '{paymentType.Name}',
-                CustomerId = '{paymentType.CustomerId}'
+            UPDATE Department
+            SET Name = '{department.Name}',
+                Budget = '{department.Budget}',
             WHERE Id = {id}";
 
             try
