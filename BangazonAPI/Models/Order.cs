fe1fc31e988b9e382c794dbf94781c8433be8f28
unknown <jonathan.d.edwards92@gmail.com>
2018-11-01 11:10:44 -0500

    can now search for inactive users

---
+++ BangazonAPI/Models/Order.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using BangazonAPI;
+
+namespace BangazonAPI.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public Customer customer { get; set; }
+        public int? PaymentTypeId { get; set; }
+        public List<Product> productList = new List<Product>();
+    }
+
+}
